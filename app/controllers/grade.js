2e26d682b4b1a8a452bfa560894f638c616f190f
carlo.i.baja <carlo.i.baja@accenture.com>
2022-01-26 23:11:07 +0800

    fix code review

--- app/controllers/grade.js
+++ app/controllers/grade.js
@@ -6,9 +6,6 @@ const getStudentGrade = (request, response) => {
     const schoolSemesterId = parseInt(request.params.schoolSemesterId)
     const schoolYearId = parseInt(request.params.schoolYearId)
     const studentNo = request.params.studentNo
-    // const schoolSemesterId = parseInt(1)
-    // const schoolYearId = parseInt(1)
-    // const studentNo = "stu1"
     pool.query('SELECT * FROM studentGrade($1,$2,$3)', [schoolSemesterId,schoolYearId,studentNo], (error, results) => {
         if (error) {
             throw error
